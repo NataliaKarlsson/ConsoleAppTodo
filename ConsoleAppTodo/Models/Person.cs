83365707501e53e56b1d5b2909d090487ca41c2f
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-09-21 14:27:49 +0200

    Made folder models and class person Todo

---
+++ ConsoleAppTodo/Models/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTodo.Models
+{
+
+
+
+    internal class Person
+    {
+        private readonly int personId;
+
+        private string? firstName;
+        private string? lastName;
+
+
+
+
+        public Person(int personId, string? firstName, string? lastName)
+        {
+            this.personId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+
+        public int PersonId
+        {
+            get
+            {
+                return personId;
+            }
+        }
+        public string? FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Firstname can´t be null or empty!");
+                }
+                else
+                {
+                    firstName = value;
+                }
+            }
+        }
+        public string? LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Lastname can´t be null or empty!");
+                }
+                else
+                {
+                    lastName = value;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
