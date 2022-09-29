b4a41ad13ee0b7188c5f63ac86a0c714fba201d0
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-09-29 18:00:30 +0200

    some changes

--- TestModels/DataTest/PersonSequencerTest.cs
+++ TestModels/DataTest/PersonSequencerTest.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleAppTodo.Data;
+using ConsoleAppTodo.Models;
 
 namespace TestConsoleAppTodo.DataTest
 {
-    internal class PersonSequencerTest
+    public class PersonSequencerTest
     {
+        PersonSequencer personSequencerTest;
+
+        public PersonSequencerTest()
+        {
+
+            personSequencerTest = new PersonSequencer();
+        }
+
+        [Fact]
+        public void ResetPersonIdTest()
+        {
+
+            //Arrange
+            PersonSequencer.Reset();
+            int expected = 0;
+            int actual;
+            actual = PersonSequencer.PersonId;
+
+            //Act
+            PersonSequencer.PersonId = 5;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NextPersonIdTest()
+        {
+            //Arrange
+            int expectedId1 = 1;
+            int expectedId2 = 2;
+            int id1;
+            int id2;
+
+            //Act
+            id1 = PersonSequencer.NextPersonId();
+            id2 = PersonSequencer.NextPersonId();
+
+
+            //Assert
+            Assert.Equal(expectedId1, id1);
+            Assert.Equal(expectedId2, id2);
+
+        }
     }
+
 }
--- TestModels/DataTest/TodoSerquencerTest.cs
+++ TestModels/DataTest/TodoSerquencerTest.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleAppTodo.Data;
 
 namespace TestConsoleAppTodo.DataTest
 {
-    internal class TodoSerquencerTest
+    public class TodoSerquencerTest
     {
+        TodoService todoService;
+
+        public TodoSerquencerTest()
+        {
+            todoService = new TodoService();
+        }
+       
+
     }
 }
